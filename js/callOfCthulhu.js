a7d0a4398b95def60517c63a3e0fb3db1dece035
SATS\kyle.hutchinson <kyle1235@gmail.com>
2018-07-09 16:36:39 -0600

    Added random character names and occupations

--- js/callOfCthulhu.js
+++ js/callOfCthulhu.js
@@ -1,18 +1,14 @@
 exports.getRandomPlayerStats = function () {
-    var values = [40, 50, 50, 50, 60, 60, 70, 80];
-    var stats = {
-        STR: undefined,
-        CON: undefined,
-        POW: undefined,
-        DEX: undefined,
-        APP: undefined,
-        SIZ: undefined,
-        INT: undefined,
-        EDU: undefined
-    };
+    var stats = {};
 
-    for (var stat in stats) {
-        stats[stat] = values.splice(getRandomIndex(values), 1)[0];
+    stats.Name = characterNames[getRandomIndex(characterNames)];
+    stats.Occupation = occupations[getRandomIndex(occupations)];
+
+    var statNames = ['STR', 'CON', 'POW', 'DEX', 'APP', 'SIZ', 'INT', 'EDU'];
+    var values = [40, 50, 50, 50, 60, 60, 70, 80];
+
+    for (var i = 0; i < statNames.length; i++) {
+        stats[statNames[i]] = values.splice(getRandomIndex(values), 1)[0];
     }
 
     return stats;
@@ -21,3 +17,88 @@ exports.getRandomPlayerStats = function () {
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
 }
+
+var characterNames = [
+    'Eaton Monahan',
+    'Stuart Jermyn',
+    'Prof. Lyman Williamson',
+    'Philip Schmidt',
+    'Lyndon Rawlings',
+    'Sheldon Capwell',
+    'Sebastian Brigham',
+    'Albert Harker',
+    'Edward Harding',
+    'Nicodemus Brandon',
+    'Tony Carter',
+    'Booker Nelson',
+    'Hiram Merriam',
+    'Hayden Glendon',
+    'Perry Updike',
+    'Lt. Edward Champney',
+    'Loren Quinlan',
+    'Milton Dewey',
+    'Nathaniel Harrison',
+    'Robert Mercer',
+    'Novella Pitman',
+    'Faye Elgin',
+    'Mildred Bowen',
+    'Eleanor Mercer',
+    'Marilyn Sterling',
+    'Neva Harker',
+    'Stella Fuller',
+    'Evelyn Devereux',
+    'Marilyn Kingston',
+    'Shirley Wilson',
+    'Theresa Ashley',
+    'Cora Gunther',
+    'Sophie Sherman',
+    'Sarah Fenner',
+    'Neva Sargent',
+    'Jane Verner',
+    'Irene Everett',
+    'Amanda Starkweather',
+    'Annie Godfrey',
+    'Novella Upton'
+];
+
+var occupations = [
+    'Accountant',
+    'Acrobat',
+    'Agency Detective',
+    'Ambassador',
+    'Antique Dealer',
+    'Architect',
+    'Artist',
+    'Author',
+    'Bank Robber',
+    'Barber',
+    'Bartender',
+    'Bible Salesman',
+    'Big Game Hunter',
+    'Book Dealer',
+    'Bookie',
+    '\“Boss\”',
+    'Bounty Hunter',
+    'Boxer',
+    'Wrestler',
+    'Burglar',
+    'Bus Driver',
+    'Catholic Priest',
+    'Clergyman',
+    'Clerk',
+    'Cocktail Waitress',
+    'Communist',
+    'Radical',
+    'Company Officer',
+    'Executive',
+    'Columnist',
+    'Con Man',
+    'Deacon',
+    'Elder',
+    'Deep-sea Diver',
+    'Dentist',
+    'Designer',
+    'Dilettante',
+    'DJ',
+    'Drifter'
+];
